d2f248d1c4a3e4fdf8af0d097c8250d24d0caab6
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-01-13 22:55:50 +0530

    Add files via upload

--- src/main/java/com/crm/qa/pages/HomePage.java
+++ src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,5 +1,28 @@
 package com.crm.qa.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.crm.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+	
+	
+	//Page factory OR:
+	@FindBy(xpath="//input[@name='email']")
+	WebElement email;
+	
+	@FindBy(xpath="//input[@name='password']")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@value='login']")
+	WebElement login;
+	
+	@FindBy(xpath="//a[@class='list-group-item'][1]")
+	WebElement register;
+	
+	@FindBy(xpath="//button[contains(text(),' Your Store')]")
+	WebElement YourStore;
+	
 
 }
